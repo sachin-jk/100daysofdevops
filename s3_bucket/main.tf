c3ab49320e13d524389e1306ff262349cf3fb8ea
sachin-jk <134486782+sachin-jk@users.noreply.github.com>
2023-12-07 04:29:07 +0530

    Update main.tf

--- s3_bucket/main.tf
+++ s3_bucket/main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-south-1"
 }
-resource "aws_s3_bucket" "example" {
-  bucket = "my-test-s3-terraform-bucket"
+resource "aws_s3_bucket" "terraform-s3-bucket" {
+  bucket = "my-test-s3-terraform-bucket627"
   acl = "private"
   versioning {
     enabled = true
   }
-  tags {
-    Name = "my-test-s3-terraform-bucket"
+  tags = {
+    Name = "my-test-s3-terraform-bucket627"
   }
 }
